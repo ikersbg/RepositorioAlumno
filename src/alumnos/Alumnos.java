bc0fcec9dd2c608bd0ae8b0ac9b8e7fa05cfeb12
ikersbg <iker.san7@educa.madrid.org>
2024-03-05 09:54:09 +0100

    Revert "Cambio el mensaje de nuevo"
    This reverts commit 77a74745642293e9bef167d7f367cbc67a1f902f.

--- src/alumnos/Alumnos.java
+++ src/alumnos/Alumnos.java
@@ -13,7 +13,7 @@ public class Alumnos {
         this.nombre = nombre;
     }
     public void Saludar(){
-        System.out.println("No me caes !!");
+        System.out.println("No me caes bien!!");
     }
     public String getNia() {
         return nia;
